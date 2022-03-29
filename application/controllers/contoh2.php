5b56b113ee3e5aec792f5a165b829780737849c5
armando <armandosianturi22@gmail.com>
2022-03-29 17:24:52 +0700

    menambahkan file matakuliah.php,view-data-matakuliah.php,view-data-matakuliah.php

--- application/controllers/contoh2.php
+++ application/controllers/contoh2.php
@@ -1,12 +1,13 @@
 <?php
-class Contoh1 extends CI_Controller
+class Contoh2 extends CI_Controller
 {
  public function index()
  {
  echo "<h1>Perkenalkan</h1>";
- echo"Nama saya adhep 
- saya tinggal di gondrong
- saya juragan sapi";
+ echo"NAMA SAYA ADHEP, 
+ SAYA TINGGAL DI GONDRONG,
+ HOBBY SAYA MELUKIS,
+ SAYA ANAK DARI JURAGAN SAPI";
  }
  }
 ?>
--- application/controllers/contoh3.php
+++ application/controllers/contoh3.php
@@ -1,10 +1,12 @@
 <?php
-class Contoh1 extends CI_Controller
+class Contoh3 extends CI_Controller
 {
  public function index()
  {
  echo "<h1>Perkenalkan</h1>";
- echo"Nama saya ";
+ echo"NAMA SAYA SULTAN ,
+ SAYA TINGGAL DI CIJANTUNG,
+ HOBBY SAYA BERMAIN FUTSAL";
  }
  }
 ?>
